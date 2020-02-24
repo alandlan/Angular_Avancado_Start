f56a433f703006891327173e8ab25502d5d34c99
Alan Vieira Martins <alanhorcrux@gmail.com>
2020-02-24 19:09:36 -0300

    Criado FormGroup e FormControl para binding info

--- Angular_Avancado_v1/Angular/src/app/demos/reactiveForms/cadastro/cadastro.component.ts
+++ Angular_Avancado_v1/Angular/src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-cadastro',
-  templateUrl: './cadastro.component.html',
-  styles: []
+  selector: "app-cadastro",
+  templateUrl: "./cadastro.component.html"
 })
 export class CadastroComponent implements OnInit {
+  cadastroForm: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.cadastroForm = new FormGroup({
+      nome: new FormControl(""),
+      cpf: new FormControl(""),
+      email: new FormControl(""),
+      senha: new FormControl(""),
+      senhaConfirmacao: new FormControl("")
+    });
   }
 
+  adicionarUsuario() {
+    let x = this.cadastroForm.value;
+  }
 }
